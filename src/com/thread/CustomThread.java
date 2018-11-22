10929e97b143037efc0050acbf2a6bcdfd60f5f0
Khang Nguyen <khangie@hotmail.com>
2018-11-21 22:09:30 -0800

    runnable thread updates

--- src/com/thread/CustomThread.java
+++ src/com/thread/CustomThread.java
@@ -4,7 +4,16 @@ public class CustomThread extends Thread {
 
 	@Override
 	public void run() {
-		System.out.println("Custom thread.");
+		System.out.println(currentThread().getName() + " - start");
+		
+		try {
+			Thread.sleep(3000);
+		} catch(InterruptedException e) {
+			System.out.println(currentThread().getName() + " - another thread woke me up");
+		}
+		
+		System.out.println(currentThread().getName() + " - 3 seconds have passed");
+		
 	}
 
 }
--- src/com/thread/ThreadMain.java
+++ src/com/thread/ThreadMain.java
@@ -11,7 +11,11 @@ package com.thread;
  * 7) A THREAD created by a PROCESS shares the PROCESS's memory and files
  * 8) This can potentially create conflicts
  * 9) Each THREAD has a THREAD STACK which is memory that only the THREAD can access
- *
+ * 
+ * Best Practices
+ * 1) Use an anonymous runnable thread to provide the most flexibility
+ * 2) Implement the run() method but always call the start() method
+ *    a) The JVM will call the run() method 
  */
 
 public class ThreadMain {
@@ -22,12 +26,25 @@ public class ThreadMain {
 	
 		// Custom thread
 		Thread customThread = new CustomThread();
+		customThread.setName("Custom Thread");
 		customThread.start();
 	
 		// Anonymous class
 		new Thread() {
 			public void run() {
 				System.out.println("Anonymous class thread");
+			}
+		}.start();
+		
+		// Using a runnable interface
+		Thread runnableThread = new Thread(new RunnableThread());
+		runnableThread.start();
+		
+		// Anonymous runnable thread
+		new Thread(new RunnableThread()) {
+			@Override
+			public void run() {
+				System.out.println("Anonymous runnable thread");
 			}
 		}.start();
 		
