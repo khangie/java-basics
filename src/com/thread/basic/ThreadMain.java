7561fd3c3674347813f9e1537bbfdff0f7b15a16
Khang Nguyen <khangie@hotmail.com>
2018-12-01 08:00:31 -0800

    Thread updates

--- src/com/thread/basic/ThreadMain.java
+++ src/com/thread/basic/ThreadMain.java
@@ -17,16 +17,21 @@ package com.thread.basic;
  *     a) When a method is SYNCHRONIZED, only one METHOD can access it at a time
  *     b) All other threads that want to call the method will suspend until the method is available
  *     c) You cannot synchronize CONSTRUCTORS because only one THREAD can construct an instance
+ *     d) SYCHRONIZING will work as a local variable only if that local variable is a STRING
+ *     e) Best to synchronize with a class variable, a string, or "this"
+ *     f) You can also synchronize STATIC variables or objects since they are owned by the class 
  * 13) Locks 
  *     a) Every object has an intrinsic LOCK
  *     b) A thread will need to acquire an object's LOCK before executing a SYNCHRONIZED statement block
- *     c) PRIMITIVE types DO NOT have an intrinsic lock
- * 
+ *     c) Only one thread can hold the LOCK at a time
+ *     d) PRIMITIVE types DO NOT have an intrinsic lock
+ * 14) When a class is THREADSAFE, then all critical sections in the class are synchronized
  * 
  * Best Practices
  * 1) Use an anonymous runnable thread to provide the most flexibility
  * 2) Implement the run() method but always call the start() method
  *    a) The JVM will call the run() method 
+ * 3) Only synchronize when necessary
  */
 
 public class ThreadMain {
