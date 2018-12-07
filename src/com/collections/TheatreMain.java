264cde20487ee94bdae57ade69379dce714ca048
Khang Nguyen <khangie@hotmail.com>
2018-12-06 19:11:45 -0800

    Added theatre for collections

---
+++ src/com/collections/TheatreMain.java
@@ -0,0 +1,24 @@
+package com.collections;
+
+public class TheatreMain {
+
+	public static void main(String[] args) {
+		
+		Theatre theatre = new Theatre("Oympian", 8, 12);
+		theatre.getSeats();
+		
+		if (theatre.reserveSeat("H11")) {
+			System.out.println("Please pay");
+		} else {
+			System.out.println("Sorry, seat is taken");
+		}
+		
+		if (theatre.reserveSeat("H11")) {
+			System.out.println("Please pay");
+		} else {
+			System.out.println("Sorry, seat is taken");
+		}
+		
+	}
+	
+}
